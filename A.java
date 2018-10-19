f0817e560290b0e10ae3367b40c40df10b1e7dff
smaranikap88 <smaranika87@gmail.com>
2018-10-19 22:55:49 +0530

    added A.java

---
+++ A.java
@@ -0,0 +1,15 @@
+package com.JavaPractice.conditions;
+
+
+class A{
+	
+	public static void main(String...abc){
+		System.out.println("Main started");
+		if(2<5)//true
+			System.out.println(2);
+	}
+}
+
+//Output:
+//Main started
+//2
